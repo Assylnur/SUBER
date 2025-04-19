3cd35b497bee1e1642303d006a52d7146f330577
Assylnur Lesken <assylnur@bu.edu>
2025-04-19 14:04:15 -0400

    rule based

--- environment/LLM/dummy_llm.py
+++ environment/LLM/dummy_llm.py
@@ -1,18 +1,18 @@
 # environment/LLM/dummy_llm.py
 from environment.LLM.llm import LLM
-
+import numpy as np
 class DummyLLM(LLM):
     def __init__(self):
         super().__init__(name="dummy")
-
+        self.previous_items_features_list = None
     def request_rating_0_9(self, system_prompt, dialog):
-        return "", "6.5"
+        return "", f"{np.random.randint(0, 10)}"
 
     def request_rating_1_10(self, system_prompt, dialog):
-        return "", "8.0"
+        return "", f"{np.random.randint(1, 11)}"
 
     def request_rating_1_5(self, system_prompt, dialog):
-        return "", "4"
+        return "", f"{np.random.randint(1, 6)}"
 
     def request_rating_text(self, system_prompt, dialog):
         return "", "seven"
